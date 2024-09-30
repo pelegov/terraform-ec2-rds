0cd76e92127adbdb9a3a8584504804ae3907e4b5
pelegov <nave.peleg@gmail.com>
2024-09-30 11:25:48 +0300

    tf files

---
+++ rds.tf
@@ -0,0 +1,12 @@
+resource "aws_db_instance" "AppDatabase" {
+  allocated_storage      = 20
+  db_name                = "appdatabase"
+  engine                 = "mysql"
+  engine_version         = "8.0.33"
+  instance_class         = "db.t3.micro"
+  username               = "admin"
+  password               = "db*pass123"
+  publicly_accessible    = true
+  db_subnet_group_name   = aws_db_subnet_group.app-db-subnet-group.name
+  vpc_security_group_ids = [aws_security_group.WebTrafficSG.id]
+}
