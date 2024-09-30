77dd9db1258d39614ff8b7aa8623c9d781e50b20
pelegov <78618051+pelegov@users.noreply.github.com>
2024-09-30 11:27:16 +0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "AppDatabase" {
   engine                 = "mysql"
   engine_version         = "8.0.33"
   instance_class         = "db.t3.micro"
-  username               = "admin"
-  password               = "db*pass123"
+  username               = "username"
+  password               = "password"
   publicly_accessible    = true
   db_subnet_group_name   = aws_db_subnet_group.app-db-subnet-group.name
   vpc_security_group_ids = [aws_security_group.WebTrafficSG.id]
